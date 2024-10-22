fab9d50dc57bb115af800a6bfe6effb81b0f9c31
Arturo <art2dit2@hotmail.com>
2024-10-22 01:49:07 -0700

    In validator we need to inject with autowired and then avoid NPE

--- src/main/java/com/arturo/springboot/security/app/springbootcrud/validation/ExistByUsername.java
+++ src/main/java/com/arturo/springboot/security/app/springbootcrud/validation/ExistByUsername.java
@@ -12,7 +12,7 @@ import java.lang.annotation.Target;
 @Retention(RetentionPolicy.RUNTIME)
 public @interface ExistByUsername {
 
-    String message() default "ya existe en este username!";
+    String message() default "Already exists";
 
     Class<?>[] groups() default {};
 
--- src/main/java/com/arturo/springboot/security/app/springbootcrud/validation/ExistUsername.java
+++ src/main/java/com/arturo/springboot/security/app/springbootcrud/validation/ExistUsername.java
@@ -1,27 +1,22 @@
 package com.arturo.springboot.security.app.springbootcrud.validation;
 
-import com.arturo.springboot.security.app.springbootcrud.repositories.UserRepository;
 import com.arturo.springboot.security.app.springbootcrud.services.UserService;
 import jakarta.validation.ConstraintValidator;
 import jakarta.validation.ConstraintValidatorContext;
-import java.util.Optional;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
 public class ExistUsername implements ConstraintValidator<ExistByUsername, String> {
 
-  private final UserService userService;
-
-  public ExistUsername(UserService userService) {
-    this.userService = userService;
-  }
+  @Autowired
+  private UserService userService;
 
   @Override
   public boolean isValid(String username, ConstraintValidatorContext context) {
     if(userService == null){
       return true;
     }
-    boolean existe = !userService.existsByUsername(username);
-    return existe;
+    return !userService.existsByUsername(username);
   }
 }
